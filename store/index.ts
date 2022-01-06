e8a49cdf3163f5f414bd95d802658efe9560a7b5
Niall <nialldbarber@gmail.com>
2022-01-06 15:41:28 +0000

    Combined reducers

--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,13 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import itemsReducer from './items.slice'
 import cartReducer from './cart.slice'
 
-export const store = configureStore({
-  reducer: {
-    allItems: itemsReducer,
-    cart: cartReducer,
-  },
+const reducer = combineReducers({
+  allItems: itemsReducer,
+  cart: cartReducer,
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+export const store = configureStore({reducer})
+
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
